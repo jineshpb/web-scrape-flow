6b584bd2c88b8f09d4f0be258d35d972fa7d6701
jineshpb <89720519+jineshpb@users.noreply.github.com>
2025-02-05 20:06:26 +0530

    puppetter fix

--- lib/puppeteer.ts
+++ lib/puppeteer.ts
@@ -1,21 +1,15 @@
 import puppeteer from "puppeteer-core";
-import chrome from "@sparticuz/chromium";
+import chromium from "@sparticuz/chromium";
 
 export async function getBrowser() {
-  const executablePath = await chrome.executablePath();
+  // Optional: If you'd like to disable webgl, true is the default
+  chromium.setGraphicsMode = false;
 
   const browser = await puppeteer.launch({
-    args: [
-      ...chrome.args,
-      "--hide-scrollbars",
-      "--disable-web-security",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-    ],
-    defaultViewport: chrome.defaultViewport,
-    executablePath: executablePath,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
     headless: true,
-    ignoreDefaultArgs: ["--disable-extensions"],
   });
 
   return browser;
