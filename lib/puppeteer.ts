9076477ce41a289186d30152f58ff2d8e3b07ff1
jineshpb <89720519+jineshpb@users.noreply.github.com>
2025-02-05 20:14:37 +0530

    direct linkfor chromium download

--- lib/puppeteer.ts
+++ lib/puppeteer.ts
@@ -2,15 +2,21 @@ import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
 export async function getBrowser() {
-  // Optional: If you'd like to disable webgl, true is the default
-  chromium.setGraphicsMode = false;
-
-  const browser = await puppeteer.launch({
-    args: chromium.args,
+  // These are the recommended args from Sparticuz/chromium
+  const options = {
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath(
+      "https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar"
+    ),
     headless: true,
-  });
+  };
 
-  return browser;
+  try {
+    const browser = await puppeteer.launch(options);
+    return browser;
+  } catch (error) {
+    console.error("Failed to launch browser:", error);
+    throw error;
+  }
 }
